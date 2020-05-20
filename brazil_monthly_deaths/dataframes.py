fbf147a7592b7a59244c0f66e5af0b9ed4168145
Pedro Bern <pedrobermoreira@gmail.com>
2020-05-20 14:21:40 -0300

    Refactor

--- brazil_monthly_deaths/dataframes.py
+++ brazil_monthly_deaths/dataframes.py
@@ -1,5 +1,6 @@
-import pandas as pd
 import os
+
+import pandas as pd
 
 dir = os.path.dirname(os.path.realpath(__file__))
 
@@ -10,7 +11,13 @@ data_2018 = pd.read_csv(dir + "/data/data_2018.csv")
 data_2019 = pd.read_csv(dir + "/data/data_2019.csv")
 data_2020 = pd.read_csv(dir + "/data/data_2020.csv")
 
-data = pd.concat(
-    [data_2015, data_2016, data_2017, data_2018, data_2019, data_2020],
-    ignore_index=True,
-)
+dfs = [
+    data_2015,
+    data_2016,
+    data_2017,
+    data_2018,
+    data_2019,
+    data_2020,
+]
+
+data = pd.concat(dfs, ignore_index=True,)
