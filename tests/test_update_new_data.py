774342f220625f3ff03325c40a0b3a4a843b5e54
Pedro Bern <pedrobermoreira@gmail.com>
2020-05-20 10:09:55 -0300

    Update test

--- tests/test_update_new_data.py
+++ tests/test_update_new_data.py
@@ -1,15 +1,25 @@
 import pandas as pd
 from brazil_monthly_deaths import update_df
 
-df1 = pd.DataFrame({"A": ["A0", "A1", "A2", "A3"], "B": ["B0", "B1", "B2", "B3"]})
-df2 = pd.DataFrame({"A": ["A0", "A1"], "B": ["B0", "B4"]})
+df1 = pd.DataFrame(
+    {
+        "A": ["A0", "A1", "A2", "A3"],
+        "B": ["B0", "B1", "B2", "B3"],
+        "C": ["C0", "C1", "C2", "C3"],
+    }
+)
+df2 = pd.DataFrame({"A": ["A0", "A1"], "B": ["B0", "B4"], "C": ["C0", "C1"]})
 
 duplicates = ["A", "B"]
 
 df3 = update_df(df1, df2, duplicates)
 
 df_correct = pd.DataFrame(
-    {"A": ["A1", "A2", "A3", "A0", "A1"], "B": ["B1", "B2", "B3", "B0", "B4"]}
+    {
+        "A": ["A1", "A2", "A3", "A0", "A1"],
+        "B": ["B1", "B2", "B3", "B0", "B4"],
+        "C": ["C1", "C2", "C3", "C0", "C1"],
+    }
 )
 
 
